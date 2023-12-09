d82afdd1909d627fa9d19126442ba0c0c9cce42b
Razmik <48383038+RazmikZakharyan@users.noreply.github.com>
2023-12-09 23:44:06 +0400

    Update views.py

--- news_project/apps/news/views.py
+++ news_project/apps/news/views.py
@@ -15,23 +15,10 @@ class ScrapeView(TemplateView):
             scrapy_task.delay()
 
         reports = []
-        reports_text = []
         for report in ReportModel.objects.all().values():
             sub_text = report['sub_text']
             report['sub_text'] = self.limit_to_50_words(sub_text)
-            if len(reports_text) == 5:
-                report['summary_report'] = self.get_summary_report(
-                    ' '.join(reports_text)
-                )
-                reports_text = []
-
-            reports_text.append(sub_text)
             reports.append(report)
-
-        if reports_text:
-            reports[-1]['summary_report'] = self.get_summary_report(
-                    ' '.join(reports_text)
-                )
 
         return self.render_to_response(
             {
@@ -39,24 +26,7 @@ class ScrapeView(TemplateView):
             }
         )
 
-    @staticmethod
-    def get_summary_report(report_text):
-        chatgpt_api_url = 'https://api.openai.com/v1/chat/completions'
-        headers = {
-            'Content-Type': 'application/json',
-            'Authorization': f'Bearer {CHATGPT_API_KEY}',
-        }
-        data = {
-            'messages': [
-                {
-                    'role': 'system',
-                    'content': 'Summarize the following report.'
-                },
-                {'role': 'user', 'content': report_text}],
-            "model": "gpt-3.5-turbo"
-        }
-        response = requests.post(chatgpt_api_url, headers=headers, json=data)
-        return response.json()['choices'][0]['message']['content']
+    
 
     @staticmethod
     def limit_to_50_words(text):
